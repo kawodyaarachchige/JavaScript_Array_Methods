15ed7b2cb9a4e99644b5a65758e14333fa2ff38b
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-06-27 13:57:54 +0530

    added join() .

--- script.js
+++ script.js
@@ -131,3 +131,11 @@ function isArray() {
     console.log(result); // Output: true
 }
 isArray()
+
+// join ()
+function join() {
+    let array = [1, 2, 3, 4, 5];
+    let result = array.join('-');
+    console.log(result); // Output: 1-2-3-4-5
+}
+join()
