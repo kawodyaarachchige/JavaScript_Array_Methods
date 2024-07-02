def688dba2a109e0d8d991abc52b99b6e6ff4f6d
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-07-02 14:10:31 +0530

    added map() .

--- script.js
+++ script.js
@@ -157,3 +157,11 @@ function lastIndexOf() {
     console.log(result); // Output: 2
 }
 lastIndexOf()
+
+// map ()
+function map() {
+    let array = [1, 2, 3, 4, 5];
+    let result = array.map((element) => element * 2);
+    console.log(result); // Output: [2, 4, 6, 8, 10]
+}
+map()
